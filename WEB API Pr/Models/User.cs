3bc33ffc5d71b47d873c9443f2a020f41c49b5b7
Mikhail Sholeninov <medvedsholeninov@yandex.ru>
2020-11-27 17:02:49 +0300

    Добавьте файлы проекта.

---
+++ WEB API Pr/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace WEB_API_Pr.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ WEB API Pr/Models/UsersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WEB_API_Pr.Models;
+
+namespace WebAPIApp.Models
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
